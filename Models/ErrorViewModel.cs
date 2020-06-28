563cfc080b061e3d8cfa0f17e41e59dd9e885a73
Nikunj Dhawan <nikunjdhawan@gmail.com>
2020-06-28 17:35:24 +0530

    firtst commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VishiArts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
